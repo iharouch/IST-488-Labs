b82002320a91e33990438f17eee16426fbe661d6
iharouch <131390371+iharouch@users.noreply.github.com>
2026-01-21 19:40:44 +0000

    created lab pages

---
+++ Labs/Lab1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Lab 1")
+st.write("This is Lab 1. Add your content here.")
---
+++ Labs/Lab2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Lab 2")
+st.write("This is Lab 2. Add your content here.")
