ba8170912d76623a4f633f8df1d89d7dbe74f4f7
iharouch <131390371+iharouch@users.noreply.github.com>
2026-01-28 21:47:19 +0000

    added the summary type option

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -20,30 +20,30 @@ uploaded_file = st.file_uploader(
     "Upload a document (.txt or .md)", type=("txt", "md")
 )
 
-# Ask the user for a question via `st.text_area`.
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
+# Provide user with three options for generating a summary
+summary_option = st.sidebar.radio("Choose a summary type:", [
+    'Summarize the document in 100 words',
+    'Summarize the document in 2 connecting paragraphs',
+    'Summarize the document in 5 bullet points'
+], disabled=not uploaded_file)
 
-if uploaded_file and question:
+if st.sidebar.button("Generate Summary", disabled=not uploaded_file):
+    if uploaded_file:
+        # Process the uploaded file
+        document = uploaded_file.read().decode()
+        messages = [
+            {
+                "role": "user",
+                "content": f"Here's a document: {document} \n\n---\n\n {summary_option}",
+            }
+        ]
 
-    # Process the uploaded file and question.
-    document = uploaded_file.read().decode()
-    messages = [
-        {
-            "role": "user",
-            "content": f"Here's a document: {document} \n\n---\n\n {question}",
-        }
-    ]
+        # Generate an answer using the OpenAI API.
+        stream = client.chat.completions.create(
+            model="gpt-5-mini",
+            messages=messages,
+            stream=True,
+        )
 
-    # Generate an answer using the OpenAI API.
-    stream = client.chat.completions.create(
-        model="gpt-5-mini",
-        messages=messages,
-        stream=True,
-    )
-
-    # Stream the response to the app using `st.write_stream`.
-    st.write_stream(stream)
+        # Stream the response to the app using `st.write_stream`.
+        st.write_stream(stream)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 lab1 = st.Page("Labs/Lab1.py", title = "Lab 1", icon="📝")
-lab2 = st.Page("Labs/Lab2.py", title = "Lab 2", icon="📝")
+lab2 = st.Page("Labs/Lab2.py", title = "Lab 2", icon="📝", default=True)
 
 pg = st.navigation([lab1, lab2])
 st.set_page_config(
