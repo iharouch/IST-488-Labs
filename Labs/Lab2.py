03d8746cd697bfb069d3a1b6a35596882059d56c
iharouch <131390371+iharouch@users.noreply.github.com>
2026-01-28 22:02:06 +0000

    Finished Lab 2 Part C

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -21,7 +21,7 @@ uploaded_file = st.file_uploader(
 )
 
 # Provide user with options for model selection
-use_advanced_model = st.sidebar.checkbox("Use advanced model (gpt-5.2)", value=False)
+use_advanced_model = st.sidebar.checkbox("Use advanced model (gpt-5.2)", value=False) # Checkbox that uses most advanced model if checked
 
 if use_advanced_model:
     model_option = 'gpt-5.2'
@@ -30,14 +30,14 @@ else:
         'gpt-4.1',
         'gpt-5-mini',
         'gpt-5.2'
-    ], index=0)
+    ], index=0) # Selectbox for model choice if not automatically using advanced model with 4.1 as default
 
 # Provide user with three options for generating a summary
 summary_option = st.sidebar.radio("Choose a summary type:", [
     'Summarize the document in 100 words',
     'Summarize the document in 2 connecting paragraphs',
     'Summarize the document in 5 bullet points'
-], disabled=not uploaded_file)
+], disabled=not uploaded_file) # Disabled until the file is uploaded
 
 if st.sidebar.button("Generate Summary", disabled=not uploaded_file):
     if uploaded_file:
@@ -48,11 +48,11 @@ if st.sidebar.button("Generate Summary", disabled=not uploaded_file):
                 "role": "user",
                 "content": f"Here's a document: {document} \n\n---\n\n {summary_option}",
             }
-        ]
+        ] # API call takes summary option into account
 
         # Generate an answer using the OpenAI API.
         stream = client.chat.completions.create(
-            model=model_option,
+            model=model_option, # Use selected model
             messages=messages,
             stream=True,
         )
