59994b58f8f56293362e80cf9ea8d71563c12b2c
iharouch <131390371+iharouch@users.noreply.github.com>
2026-02-04 18:16:58 +0000

    created lab 3 file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 lab1 = st.Page("Labs/Lab1.py", title = "Lab 1", icon="📝")
-lab2 = st.Page("Labs/Lab2.py", title = "Lab 2", icon="📝", default=True)
+lab2 = st.Page("Labs/Lab2.py", title = "Lab 2", icon="📝")
+lab3 = st.Page("Labs/Lab3.py", title = "Lab 3", icon="📝", default=True)
 
-pg = st.navigation([lab1, lab2])
+pg = st.navigation([lab1, lab2, lab3])
 st.set_page_config(
    # Set page title
    page_title = "IST 488 Lab App", 
