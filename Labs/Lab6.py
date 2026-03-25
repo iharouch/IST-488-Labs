bd4306cb84055bd6ee3ccab2adfc343d0dd992a1
Ines Harouchi <iharouch@syr.edu>
2026-03-24 22:48:16 -0400

    lab 6 part b

--- Labs/Lab6.py
+++ Labs/Lab6.py
@@ -19,3 +19,23 @@ if prompt:
     )
 
     st.write(response.output_text)
+
+    #Store response ID
+    st.session_state.last_response_id = response.id
+
+    if "last_response_id" in st.session_state:
+        follow_up = st.text_input("Ask a follow-up question:", key="follow_up")
+    
+    if "last_response_id" in st.session_state and follow_up:
+        follow_response = client.responses.create(
+            model="gpt-4.1",
+            instructions="You are a helpful teaching assistant for an R course.",
+            input=follow_up,
+            previous_response_id=st.session_state.last_response_id
+        )
+
+        st.write(follow_response.output_text)
+
+        # Update last response variable for further follow-up questions
+        st.session_state.last_response_id = follow_response.id
+
