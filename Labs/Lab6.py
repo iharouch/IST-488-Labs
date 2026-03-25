af4a31c2f24d25e92aff7325199f5c46c7eb8313
Ines Harouchi <iharouch@syr.edu>
2026-03-24 22:24:14 -0400

    fixed errors

--- Labs/Lab6.py
+++ Labs/Lab6.py
@@ -7,13 +7,15 @@ if 'client' not in st.session_state:
     st.session_state.client = OpenAI(api_key=api_key)
 
 st.title("Responses API Chatbot")
+
 prompt = st.text_input("Ask me a question!")
 client = st.session_state.client
 
-response = client.responses.create(
-    model="gpt-4o",
-    instructions="You are a helpful teaching assistant for an R course.",
-    input=prompt
-)
+if prompt:
+    response = client.responses.create(
+        model="gpt-4.1",
+        instructions="You are a helpful teaching assistant for an R course.",
+        input=prompt
+    )
 
-print(response.output_text)
+    st.write(response.output_text)
