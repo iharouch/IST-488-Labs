5659b1fdbafc25211be3c22a95f3921527471cba
Ines Harouchi <iharouch@syr.edu>
2026-04-06 13:17:41 -0400

    added lab 8 page

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,10 @@ lab2 = st.Page("Labs/Lab2.py", title = "Lab 2", icon="📝")
 lab3 = st.Page("Labs/Lab3.py", title = "Lab 3", icon="📝")
 lab4 = st.Page("Labs/Lab4.py", title = "Lab 4", icon="📝")
 lab5 = st.Page("Labs/Lab5.py", title = "Lab 5", icon="📝")
-lab6 = st.Page("Labs/Lab6.py", title = "Lab 6", icon="📝", default=True)
+lab6 = st.Page("Labs/Lab6.py", title = "Lab 6", icon="📝")
+lab8 = st.Page("Labs/Lab8.py", title = "Lab 8", icon="📝", default=True)
 
-pg = st.navigation([lab1, lab2, lab3, lab4, lab5, lab6])
+pg = st.navigation([lab1, lab2, lab3, lab4, lab5, lab6, lab8])
 st.set_page_config(
    # Set page title
    page_title = "IST 488 Lab App", 
